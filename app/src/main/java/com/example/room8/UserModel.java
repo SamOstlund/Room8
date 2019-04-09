ab0439feceb7526157d4574d5bb92876b0e362ba
Purghatory <jg15w@my.fsu.edu>
2019-04-09 14:43:00 -0400

    Registration page creates valid user and stores user information nested under Users/(userID) in firebase realtime database

--- app/src/main/java/com/example/room8/UserModel.java
+++ app/src/main/java/com/example/room8/UserModel.java
@@ -2,18 +2,22 @@ package com.example.room8;
 
 public class UserModel {
 
-    String name, email, password;
+    String firstName, lastName, email, password, age, minprice, maxprice, zip, bio;
 
-    public UserModel(){
-
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getName() {
-        return name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     public String getEmail() {
@@ -31,4 +35,50 @@ public class UserModel {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getMinprice() {
+        return minprice;
+    }
+
+    public void setMinprice(String minprice) {
+        this.minprice = minprice;
+    }
+
+    public String getMaxprice() {
+        return maxprice;
+    }
+
+    public void setMaxprice(String maxprice) {
+        this.maxprice = maxprice;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public UserModel(){
+
+    }
+
+
 }
--- app/src/main/java/com/example/room8/registerActivity.java
+++ app/src/main/java/com/example/room8/registerActivity.java
@@ -1,11 +1,23 @@
 package com.example.room8;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class registerActivity extends AppCompatActivity {
@@ -14,13 +26,17 @@ public class registerActivity extends AppCompatActivity {
     private EditText firstName, lastName, email, password, age, minprice, maxprice, zip, bio;
     private Button submitRegisterButton, registerBack;
 
-    com.google.firebase.database.DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
+    DatabaseReference databaseReference;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
 
         firstName = (EditText) findViewById(R.id.firstName);
         lastName = (EditText) findViewById(R.id.lastName);
@@ -40,7 +56,13 @@ public class registerActivity extends AppCompatActivity {
         submitRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AddData();
+                Register();
+
+                /*AddData();
+                if (isEmailValid(email.getText().toString().trim()) && isPasswordValid(password.getText().toString().trim())){
+                    Register();
+                }
+                */
 
             }
         });
@@ -51,6 +73,53 @@ public class registerActivity extends AppCompatActivity {
                 startActivity(new Intent(registerActivity.this, LoginActivity.class));
             }
         });
+    }
+
+    private void Register(){
+
+        mAuth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    final FirebaseUser user = task.getResult().getUser();
+                    if (user != null){
+                        mAuth.signInWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()){
+                                    com.google.firebase.database.DatabaseReference databaseReference = com.google.firebase.database.FirebaseDatabase.getInstance().getReference().child("Users");
+                                    databaseReference = databaseReference.child(user.getUid());
+
+                                    UserModel userModel = new UserModel();
+
+                                    userModel.setFirstName(firstName.getText().toString().trim());
+                                    userModel.setLastName(lastName.getText().toString().trim());
+                                    userModel.setEmail(email.getText().toString().trim());
+                                    userModel.setPassword(password.getText().toString().trim());
+                                    userModel.setAge(age.getText().toString().trim());
+                                    userModel.setMinprice(minprice.getText().toString().trim());
+                                    userModel.setMaxprice(maxprice.getText().toString().trim());
+                                    userModel.setZip(zip.getText().toString().trim());
+                                    userModel.setBio(bio.getText().toString().trim());
+
+                                    databaseReference.setValue(userModel);
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
     }
 
 
