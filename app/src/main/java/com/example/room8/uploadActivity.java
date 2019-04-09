da17f175fc63460aace4a09a75a75a3c0f62359b
Purghatory <jg15w@my.fsu.edu>
2019-04-09 18:10:30 -0400

    Update to upload activity to get it to show up

--- app/src/main/java/com/example/room8/uploadActivity.java
+++ app/src/main/java/com/example/room8/uploadActivity.java
@@ -1,6 +1,8 @@
 package com.example.room8;
 
 import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +15,9 @@ public class uploadActivity extends AppCompatActivity {
 
     private final int PICK_IMAGE_REQUEST = 71;
 
-
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
 }
