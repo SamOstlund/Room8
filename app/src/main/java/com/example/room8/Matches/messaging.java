8507d8a9b20f5faf2bee021a57047b5d322e78b6
Alex <alexchiciu98@live.com>
2019-04-27 22:55:13 -0400

    Implementing display matches

--- app/src/main/java/com/example/room8/Matches/messaging.java
+++ app/src/main/java/com/example/room8/Matches/messaging.java
@@ -1,12 +1,17 @@
-package com.example.room8;
+package com.example.room8.Matches;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.room8.MainActivity;
+import com.example.room8.R;
+import com.example.room8.profileActivity;
 
 
 
@@ -15,9 +20,12 @@ import android.view.MenuItem;
 
 public class messaging extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_messaging);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
@@ -28,8 +36,7 @@ public class messaging extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch(menuItem.getItemId())
-                {
+                switch (menuItem.getItemId()) {
                     case R.id.navigation_profile:
                         Intent intent = new Intent(messaging.this, profileActivity.class);
                         startActivity(intent);
@@ -47,5 +54,14 @@ public class messaging extends AppCompatActivity {
                 return false;
             }
         });
+
+/*
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setNestedScrollingEnabled(false);
+        mRecyclerView.setHasFixedSize(true);
+*/
+
     }
+
+
 }
